71284b3c5d78681771451e51775380e934bf70bd
albjongjuzi <albjon.gjuzi@wearefiber.com>
2022-09-02 15:36:23 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { CardsComponent } from './cards/cards.component'
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ValueToTextPipe } from './pipes/value-to-text.pipe'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, CardsComponent, ValueToTextPipe],
+	imports: [BrowserModule, AppRoutingModule, DragDropModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
